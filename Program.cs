f08cc7dbe97e8cca0ec45be939a353f0c470230b
Toast <76986735+Blixtzz@users.noreply.github.com>
2024-10-31 17:30:03 -0700

    main

--- Program.cs
+++ Program.cs
@@ -1,34 +1,20 @@
-﻿using System;
+﻿﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Configuration;
-using System.Collections.Specialized;
 
 namespace SocketCshart
 {
     class Program
     {
-        static string serverIP = "127.0.0.1";
-        static int serverPort = 9000;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string process;
-            AlignBytes._sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            var addresses = Dns.GetHostAddresses(serverIP);
-            AlignBytes._remoteIpEndPoint = new IPEndPoint(addresses[0], serverPort);
-            Console.WriteLine("enter the desired delay between sends");
-            var delay = Console.ReadLine();
-            Console.WriteLine("enter the name of the process you would like to collect");
-            process = Console.ReadLine();
-            while (true)
-            {
-                if (int.Parse(delay)== 0)
-                {
-                    VRChatFunctions.OnNewSong();
+            AlignBytes.Startup();
+            string authCode = await Auth.GetAuthorizationCodeAsync();
+            if (!string.IsNullOrEmpty(authCode)) {
+                await Auth.InitializeTokensAsync(authCode);
+            while(true) {
+                await VRChatFunctions.ToastString(2000);
                 }
-                else
-                    VRChatFunctions.ToastString(process, int.Parse(delay));
             }
         }
     }
